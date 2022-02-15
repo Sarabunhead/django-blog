f118818017af1f97929fd4d0c6385b5276bf4243
idtop Apple <idtopapple@idtops-MacBook-Pro.local>
2022-02-15 12:41:55 +0330

    urls.py add comment

--- ablog/urls.py
+++ ablog/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    # this url for admin panele
     path('admin/', admin.site.urls),
     path('', include('book.urls')),
     path('members/', include('django.contrib.auth.urls')),
